4757c20f46e4f81bd596c101b54b8bfaee40ce6a
Ohunjon10 <mahmudovohunjon280@gmail.com>
2022-05-31 05:39:47 -0400

    commit

---
+++ src/components/Board/Board.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Board.css";
+import Cell from '../Cell/Cell'
+
+const Board = (props) => {
+  const cells = props.cellValues.map((value, index) => {
+    const canHighlight = props.winningCombination && 
+                         props.winningCombination.indexOf(index) >= 0;
+    
+    return <Cell 
+            key={index}
+            value={value} 
+            canHighlight={canHighlight} 
+            onClick={() => props.cellClicked(index)}
+           />;
+  });
+
+  return (
+    <div id="board">
+      {cells}
+    </div>
+  );
+};
+
+export default Board;
